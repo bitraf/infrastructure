aa5f4ae26015da88431c179d16e0efa50335e87c
Trygve Laugstøl <trygvis@inamo.no>
2021-02-11 20:33:31 +0100

    wip

---
+++ ops/terraform/main.tf
@@ -0,0 +1,43 @@
+# Tokens are configured by .settings.sh
+terraform {
+  backend "s3" {
+    bucket                      = "bitraf-terraform"
+    key                         = "root/terraform.tfstate"
+    region                      = "eu-central-1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    endpoint                    = "eu-central-1.linodeobjects.com"
+  }
+
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      version = "1.13.4"
+    }
+
+    minio = {
+      source = "aminueza/minio"
+      version = "1.2.0"
+    }
+  }
+}
+
+provider "minio" {
+  minio_server = "bite.bitraf.no:9000"
+#  minio_ssl = "true"
+  minio_access_key = var.minio_access_key
+  minio_secret_key = var.minio_secret_key
+}
+
+variable "minio_access_key" {
+  type = string
+}
+
+variable "minio_secret_key" {
+  type = string
+}
+
+module "dns" {
+  source = "./dns"
+}
---
+++ ops/terraform/minio.tf
@@ -0,0 +1,11 @@
+module "pg-backup-bite" {
+  source = "./modules/pg-backup"
+  id = "bite"
+}
+
+output "pg_backup_bite" {
+  value = {
+    sender: module.pg-backup-bite.sender,
+    bucket: module.pg-backup-bite.bucket,
+  }
+}
