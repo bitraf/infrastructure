659fd353a2f58679d9859915af104def364400cd
Trygve Laugstøl <trygvis@inamo.no>
2021-06-01 10:53:09 +0200

    ops/terraform/dns: using linode for storage

--- ops/terraform/dns/main.tf
+++ ops/terraform/dns/main.tf
@@ -1,4 +1,15 @@
+# Tokens are configured by .settings.sh
 terraform {
+  backend "s3" {
+    bucket                      = "bitraf-terraform"
+    key                         = "dns/terraform.tfstate"
+    region                      = "eu-central-1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    endpoint                    = "eu-central-1.linodeobjects.com"
+  }
+
   required_providers {
     linode = {
       source  = "linode/linode"
