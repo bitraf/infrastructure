a517130b588b14951f0bd906b39bd17e95b3d4f5
Trygve Laugstøl <trygvis@inamo.no>
2021-08-16 19:25:55 +0200

    p2k16-production

---
+++ ops/terraform/p2k16-production-db/main.tf
@@ -0,0 +1,51 @@
+# Tokens are configured by .settings.sh
+terraform {
+  backend "s3" {
+    bucket                      = "bitraf-terraform"
+    key                         = "p2k16-production-db/terraform.tfstate"
+    region                      = "eu-central-1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    endpoint                    = "eu-central-1.linodeobjects.com"
+  }
+
+  required_providers {
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = "1.13.0"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "2.1.0"
+    }
+  }
+}
+
+data "terraform_remote_state" "pdb" {
+  backend = "s3"
+
+  config = {
+    bucket                      = "bitraf-terraform"
+    key                         = "pdb/terraform.tfstate"
+    region                      = "eu-central-1"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    endpoint                    = "eu-central-1.linodeobjects.com"
+  }
+}
+
+module "p2k16-db" {
+  source            = "../modules/p2k16-db"
+  postgres_password = data.terraform_remote_state.pdb.outputs.pdb-p2k16-production_vault.pdb_postgres_password
+  host              = "p2k16-production.lan.bitraf.no"
+  env               = "production"
+}
+
+module "terraform-output" {
+  source = "../modules/terraform-output"
+  name   = "p2k16-production-db"
+  public = module.p2k16-db.public
+  vault  = module.p2k16-db.vault
+}
--- ops/terraform/pdb/pdb.tf
+++ ops/terraform/pdb/pdb.tf
@@ -33,24 +33,33 @@ module "terraform-output-pdb-13" {
 }
 
 #
-# p2k16-prod
+# p2k16-production
 #
 
-resource "random_uuid" "p2k16-prod-postgres_password" {
+resource "random_uuid" "p2k16-production-postgres_password" {
 }
 
-module "pdb-p2k16-prod" {
+module "pdb-p2k16-production" {
   source = "../modules/pdb-bucket"
 
-  key = "pdb-p2k16-prod"
-  postgres_password = random_uuid.p2k16-prod-postgres_password.result
+  key = "pdb-p2k16-production"
+  postgres_password = random_uuid.p2k16-production-postgres_password.result
 }
 
-module "terraform-output-pdb-p2k16-prod" {
+module "terraform-output-pdb-p2k16-production" {
   source = "../modules/terraform-output"
-  name   = "pdb/pdb-p2k16-prod"
-  public = module.pdb-p2k16-prod.pdb_config_public
-  vault = module.pdb-p2k16-prod.pdb_config_vault
+  name   = "pdb/pdb-p2k16-production"
+  public = module.pdb-p2k16-production.pdb_config_public
+  vault = module.pdb-p2k16-production.pdb_config_vault
+}
+
+output "pdb-p2k16-production_public" {
+  value = module.pdb-p2k16-production.pdb_config_public
+}
+
+output "pdb-p2k16-production_vault" {
+  value = module.pdb-p2k16-production.pdb_config_vault
+  sensitive = true
 }
 
 #
@@ -72,4 +81,13 @@ module "terraform-output-pdb-p2k16-staging" {
   name   = "pdb/pdb-p2k16-staging"
   public = module.pdb-p2k16-staging.pdb_config_public
   vault = module.pdb-p2k16-staging.pdb_config_vault
+}
+
+output "pdb-p2k16-staging_public" {
+  value = module.pdb-p2k16-staging.pdb_config_public
+}
+
+output "pdb-p2k16-staging_vault" {
+  value = module.pdb-p2k16-staging.pdb_config_vault
+  sensitive = true
 }
