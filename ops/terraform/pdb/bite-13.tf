01f4ea89b7d0158b847e99ee9bc1cc8f1d34584a
Trygve Laugstøl <trygvis@inamo.no>
2021-08-28 22:43:04 +0200

    pdb: Reinstalling
    * Using new bitraf postgresql images by default.
    * Removing pdb-10, renaming pdb-13 to 'bite-13'. Since we're not going
      with one big instance for everyone, inclue the hostname. bite-13 is
      still the instance that most systems should use unless they have
      specific reasons not not.

--- ops/terraform/pdb/bite-13.tf
+++ ops/terraform/pdb/bite-13.tf
@@ -14,3 +14,12 @@ module "terraform-output-bite-13" {
   public = module.bite-13.pdb_config_public
   vault  = module.bite-13.pdb_config_vault
 }
+
+output "pdb-bite-13_public" {
+  value = module.bite-13.pdb_config_public
+}
+
+output "pdb-bite-13_vault" {
+  value     = module.bite-13.pdb_config_vault
+  sensitive = true
+}
